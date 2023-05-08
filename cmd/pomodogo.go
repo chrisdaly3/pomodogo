4ab3d73b650819fe2b98ddf7479c8910998b3b33
Chris Daly <cdaly@urbn.com>
2023-05-08 19:12:30 -0400

    Update code comments on package main

--- cmd/pomodogo.go
+++ cmd/pomodogo.go
@@ -13,16 +13,16 @@ import (
 func main() {
   // Initialize a new parser object
   parser := argparse.NewParser("Start", "Creates a new pomodoro timer")
-  // Create a string flag
+  // Create int and string flags, respectively
   timeLength := parser.Int("t", "time", &argparse.Options{Required: false, Help: "Set a time-length for the working session", Default: 25})
   goal := parser.String("g", "goal", &argparse.Options{Required: true, Help: "Set a goal for this pomodoro session", Default: ""})
-  // Parse input
+  // Parse input for args
   err := parser.Parse(os.Args)
   if err != nil {
     // In case of error print error + usage
-    // Can also be achieved through -h or --help flags
+    // this is also returned in the event of the -h / --help flag
     panic(parser.Usage(err))
   }
-  // Now print the collected string
+  // Begin a new timer based on input from flags
   ticker.NewPomodoro(int(*timeLength),string(*goal)) 
 }
